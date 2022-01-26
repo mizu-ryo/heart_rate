cf25dae4aab36d4cc500adc588bfe064b273431d
mizu-ryo <mukusuke421@gmail.com>
2022-01-26 16:54:45 +0900

    3

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var myCharacteristic;
 
-var hrs = [];
 var hr = 0;
+var hr_sum = 0;
+var hr_ave = 0;
+var hr_max = 0;
+var hr_min = 0;
+
 var ex_flg = false;
 var flg = false;
 var count = 0;
+var ex_count = 0;
 var output = [['ex_time','heart_rate','flg']];
-var output_csv = '';
+var output_csv = 'ex_time,heart_rate,flg\n';
 
 function onStartButtonClick() {
     //alert('Javascriptを使ってアラートを表示しています！');
@@ -62,28 +67,83 @@ function handleNotifications(event) {
       //a.push('0x' + ('00' + value.getUint8(i).toString(16)).slice(-2));
       a.push(value.getUint8(i));
     }
+
+    count++;
     hr = a[1];
-    hrs.push(hr);
-    let temp = [];
+    hr_sum += hr;
+    hr_ave = hr_sum / count;
+
+    if(count == 1){
+        hr_max = hr;
+        hr_min = hr;
+    }
+    if(hr > hr_max){
+        hr_max = hr;
+    }
+    if(hr < hr_min){
+        hr_min = hr;
+    } 
+    //console.log(hr_sum,hr_ave.toFixed(2),hr_max,hr_min)
+
     if(ex_flg == true){
-        count = count + 1;
-        let time = ('00' + Math.floor(count/60)).slice(-2) + ':' + ('00' + count%60).slice(-2)
-        temp.push(time);
-        temp.push(hr);
+        ex_count++;
+        let time = ('00' + Math.floor(ex_count/60)).slice(-2) + ':' + ('00' + ex_count%60).slice(-2)
+
+        output_csv += time+ ',' + hr + ',';
         if(flg == true){
-            temp.push(true);
+            output_csv += true
             flg = false;
         }else{
-            temp.push('')
+            output_csv += ''
+            
         }
-        output.push(temp);
-        console.log(output);
+        output_csv += '\n'
         document.getElementById('ex_time').innerHTML = time ;
     }
-    
 
-    document.getElementById('heart_rate_now').innerHTML = hr.toString() ; 
-    console.log('> ' + a.join(' '));
+    document.getElementById('heart_rate_now').innerHTML = hr ;
+    document.getElementById('heart_rate_ave').innerHTML = hr_ave.toFixed(2) ;
+    document.getElementById('heart_rate_max').innerHTML = hr_max ;
+    document.getElementById('heart_rate_min').innerHTML = hr_min ; 
+    document.getElementById('log').innerHTML = output_csv ;
+    //console.log('> ' + a.join(' '));
+}
+
+
+
+function ontestClick() {
+    
+    //console.log(output);
+    //console.log(output.length)
+    for (let i = 0; i < output.length; i++){
+        //console.log(output[i]);
+        for (let j = 0; j < output[i].length; j++){
+            //console.log(output[i][j])
+            output_csv += output[i][j]
+            output_csv += ','
+        }
+        output_csv += '\n'
+    }
+    //console.log(output_csv)
+    //console.log(typeof(output_csv))
+
+    document.getElementById('log').innerHTML = output_csv ;
+}
+
+function onExStartClick() {
+    ex_flg = true;
+    console.log('ex_flg true')
+}
+
+function onExStopClick() {
+    ex_flg = false;
+    console.log('ex_flg false')
+}
+
+function ontrueClick() {
+    flg = true;
+    console.log('flg true')
+    //document.getElementById('flg').innerHTML = 'yes' ;
 }
 
 function onDownloadClick() {
@@ -99,7 +159,7 @@ function onDownloadClick() {
                 + ('00' + day.toString()).slice(-2)
                 + ('00' + hour.toString()).slice(-2) 
                 + ('00' + minutes.toString()).slice(-2) ;
-    console.log(now_time);
+    //console.log(now_time);
     
     // create a temporary "a" element.
     const a = document.createElement("a");
@@ -113,58 +173,4 @@ function onDownloadClick() {
     a.click();
     window.URL.revokeObjectURL(url); // release the used object.
     a.parentNode.removeChild(a); // delete the temporary "a" element
-    
-}
-
-function ontestClick() {
-    
-    console.log(output);
-    console.log(output.length)
-    for (let i = 0; i < output.length; i++){
-        console.log(output[i]);
-        for (let j = 0; j < output[i].length; j++){
-            console.log(output[i][j])
-            output_csv += output[i][j]
-            output_csv += ','
-        }
-        output_csv += '\n'
-    }
-    //console.log(output_csv)
-    //console.log(typeof(output_csv))
-
-    document.getElementById('log').innerHTML = output_csv ;
-
-    
-}
-
-function ontrueClick() {
-    flg = true;
-    console.log('flg true')
-}
-
-function onexstartClick() {
-    ex_flg = true;
-    console.log('flg true')
-}
-function onexstopClick() {
-    ex_flg = false;
-    console.log('flg true')
-}
-
-function ondateClick() {
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1; //月が代入される
-    var day = date.getDate()
-    var hour = date.getHours();
-    var minutes = date.getMinutes();
-    //const seconds = date.getSeconds();
-    //const milliseconds = date.getMilliseconds();
-    //console.log(year,month,day,hour,minutes,seconds,milliseconds);
-    var now_time = year.toString().slice(-2)
-                + ('00' + month.toString()).slice(-2)
-                + ('00' + day.toString()).slice(-2)
-                + ('00' + hour.toString()).slice(-2) 
-                + ('00' + minutes.toString()).slice(-2) ;
-    console.log(now_time);
 }
